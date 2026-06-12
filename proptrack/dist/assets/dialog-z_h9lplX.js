c308a29e464f9d5e489db5899d3809248e47e3b3
Andrew Woelfel <andrew.woelfel22@gmail.com>
2026-06-12 17:41:40 -0400

    various updates

--- proptrack/dist/assets/dialog-z_h9lplX.js
+++
@@ -1,1 +0,0 @@
-import{i as a}from"./index-gumbIqRd.js";async function i(e={}){return typeof e=="object"&&Object.freeze(e),a({__tauriModule:"Dialog",message:{cmd:"saveDialog",options:e}})}export{i as save};
